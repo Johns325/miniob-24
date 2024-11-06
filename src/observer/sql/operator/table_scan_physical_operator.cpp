f5959e66240b8b4315245e73b1e97d18c5ca20c7
Tingwk <Tingwk#github.com>
2024-11-06 19:33:05 +0800

    enable sql_debug to print some useful message so that later i can find out what's the problem with complex-sub-query

--- src/observer/sql/operator/table_scan_physical_operator.cpp
+++ src/observer/sql/operator/table_scan_physical_operator.cpp
@@ -71,7 +71,7 @@ RC TableScanPhysicalOperator::next()
     }
 
     if (filter_result) {
-      //sql_debug("get a tuple: %s from table %s", tuple_.to_string().c_str(), table_->name());
+      sql_debug("get a tuple: %s from table %s", tuple_.to_string().c_str(), table_->name());
       break;
     } else {
      // sql_debug("a tuple is filtered: %s from table %s", tuple_.to_string().c_str(), table_->name());
