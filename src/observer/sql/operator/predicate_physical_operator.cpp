4e9a1cddec5d83d80df2ed45e9585debc522cf07
Tingwk <Tingwk#github.com>
2024-11-09 09:32:37 +0800

    I want to know the bug comes from BplusTree index or not

--- src/observer/sql/operator/predicate_physical_operator.cpp
+++ src/observer/sql/operator/predicate_physical_operator.cpp
@@ -229,7 +229,7 @@ RC PredicatePhysicalOperator::next()
     }
 
     if (value.get_boolean()) {
-      sql_debug("get a tuple: %s ", tuple->to_string().c_str());
+      // sql_debug("get a tuple: %s ", tuple->to_string().c_str());
       return rc;
     }
   }
